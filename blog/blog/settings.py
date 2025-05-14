338567ec716949a1c1329026764b61d7f74653af
ramo1990 <abudul917@gmail.com>
2025-05-15 00:25:44 +0200

    Permettre a certains user de modifier ou  supprimer leur post

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -109,7 +109,9 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # pour la prod
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
