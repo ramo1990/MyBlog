75dc2bb4d3f5b0052d20e2ea15cc7716d9e48309
ramo1990 <abudul917@gmail.com>
2025-05-17 23:24:49 +0200

    Deploiement initial

--- blog/app/urls.py
+++ blog/app/urls.py
@@ -11,13 +11,14 @@ urlpatterns = [
     path('post/<slug:slug>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('destinations/', DestinationListView.as_view(), name='destination_list'),
     path('destinations/<slug:slug>/', DestinationDetailView.as_view(), name='destination_detail'),
-    path('culture/', culture_view, name='culture'),
+
+    path('culture/', CultureListView.as_view(), name='culture'),
+    path('culture/<slug:slug>/', CultureDetailView.as_view(), name='culture_detail'),
+
     path('conseils/', conseils_view, name='conseils'),
     path('contact/', contact_view, name='contact'),
     path('a-propos/', a_propos_page, name='a_propos'),
-
     path('i18n/setlang/', set_language, name='set_language'),  # ✅ permet à reverse('set_language') de fonctionner
-
     path('<slug:slug>/', DetailView.as_view(), name='post_detail'),
 ]
 
