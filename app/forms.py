a319a7a7f20b79392d0b1b2afc8f0f752e32bbcb
ramo1990 <abudul917@gmail.com>
2025-05-19 10:49:32 +0200

    Ajout du  fichier render.yaml

---
+++ app/forms.py
@@ -0,0 +1,45 @@
+# from django import forms
+# from .models import Post, PostImage
+# from django.forms import modelformset_factory
+
+
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         # fields = ['title', 'content', 'status', 'image']
+#         exclude = ['author', 'slug']
+
+# PostImageFormSet = modelformset_factory(
+#     PostImage,
+#     fields=('image',),
+#     extra=0,
+#     can_delete=True
+# )
+
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+
+# class SignUpForm(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput())
+#     password_confirm = forms.CharField(widget=forms.PasswordInput())
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'first_name', 'last_name']
+
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password = cleaned_data.get('password')
+#         password_confirm = cleaned_data.get('password_confirm')
+
+#         if password != password_confirm:
+#             raise forms.ValidationError('Les mots de passe ne correspondent pas.')
+
+#         return cleaned_data
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = MessageContact
+        fields = ['nom', 'email', 'sujet', 'message']
+        widgets = {'message':forms.Textarea(attrs={'rows':4}),}
